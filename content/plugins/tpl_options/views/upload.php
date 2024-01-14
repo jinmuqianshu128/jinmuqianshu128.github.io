f97ad0871e9de2f5872700bb062b6de55b672757
锦木千束 <weizihao128@outlook.com>
2024-01-14 16:36:12 +0800

    Delete content directory

--- content/plugins/tpl_options/views/upload.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('EMLOG_ROOT') || exit('access denied!');
-?>
-<script>
-    parent.setImage('<?php echo $src; ?>', '<?php echo $path; ?>', <?php echo $code; ?>, '<?php echo $this->encode($msg); ?>');
-</script>
